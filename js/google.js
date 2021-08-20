0c228d1367ca73215b95f65c94236bfdaef4beee
maxval0386 <88293942+maxval0386@users.noreply.github.com>
2021-08-19 22:24:05 -0300

    Cambios 2

--- js/google.js
+++ js/google.js
@@ -10,9 +10,28 @@ function onSignIn(googleUser) {
       location.href = "index.html";
     }
 
-    // function signOut() {
-    //   var auth2 = gapi.auth2.getAuthInstance();
-    //   auth2.signOut().then(function(){
-    //     console.log("User signed out.");
-    //   });
-    // }
+    function signOut() {
+      var auth2 = gapi.auth2.getAuthInstance();
+      auth2.signOut().then(function(){
+        console.log("User signed out.");
+      });
+    }
+
+    function onLoad() {
+      gapi.load('auth2', function() {
+        gapi.auth2.init();
+      });
+    }
+    
+    function desconectar() {
+      var estado=localStorage.getItem("pass");
+      if (estado==null) {
+        localStorage.clear();
+        sessionStorage.clear();
+        signOut();
+      }
+      else{
+        localStorage.setItem("estado", "false");
+      }
+      location.href="login.html";
+    }
