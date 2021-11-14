5d39179e387d27efada5fc758f7018dc7ef4e28b
maxval0386 <88293942+maxval0386@users.noreply.github.com>
2021-11-14 01:35:07 -0300

    Obliatorio 7

--- js/cart.js
+++ js/cart.js
@@ -1,29 +1,31 @@
 let cart_info_array = [];
 var cotizacion = 40;
 var dolar = false;
-
+var metodoDePagoSeleccionado = false;
 
 // Muestra los productos agregados en el carrito
 function showCartList() {
-    let cart = cart_info_array;
-    let listCart = document.getElementById("listCart");
-    let htmlContentToAppend = "";
-  
-    if (cart.length > 0) {
-      var moneda = document.getElementById("productCurrency").value;
-      for (let i = 0; i < cart.length; i++) {
-        let item = cart[i];
-        var costo = item.unitCost
-        if (moneda != item.currency) {
-          costo = cambMoneda(item.currency, costo, moneda)
-        }
-  
-        htmlContentToAppend += `
+  let cart = cart_info_array;
+  let listCart = document.getElementById("listCart");
+  let htmlContentToAppend = "";
+
+  if (cart.length > 0) {
+    var moneda = document.getElementById("productCurrency").value;
+    for (let i = 0; i < cart.length; i++) {
+      let item = cart[i];
+      var costo = item.unitCost;
+      if (moneda != item.currency) {
+        costo = cambMoneda(item.currency, costo, moneda);
+      }
+
+      htmlContentToAppend += `
             <li id="item-${i}" class="list-group-item">
               <div class="row">
       
                 <div class="col-6 col-sm-4 col-md-4 col-lg-4 col-xl-4 mx-auto">
-                  <img src="${item.src}" alt="xc" class="img-thumbnail mx-auto d-block">
+                  <img src="${
+                    item.src
+                  }" alt="xc" class="img-thumbnail mx-auto d-block">
                 </div>
       
                 <div class="col-12 col-sm align-self-stretch d-flex flex-column">
@@ -45,12 +47,16 @@ function showCartList() {
                             <label for="cant${i}" class="pr-2">Cantidad</label>
                           </div>
                           <div class="col-5 col-sm-6 col-md-5 col-lg-6 col-xl-3">
-                            <input type="number" class="form-control" id="cant${i}" name="cant" required size="6" value="${item.count}" min="1" max="1000" oninput="actualizaCalc(${i})">
+                            <input type="number" class="form-control" id="cant${i}" name="cant" required size="6" value="${
+        item.count
+      }" min="1" max="1000" oninput="actualizaCalc(${i})">
                           </div>
                         </div>
                       </div>
                       <div class="col pl-0 text-right">
-                        <span id="productCostText${i}" class="text-muted precio-subtotal">${moneda} ${costo * item.count}</span>
+                        <span id="productCostText${i}" class="text-muted precio-subtotal">${moneda} ${
+        costo * item.count
+      }</span>
                       </div>
                     </div>
                   </div>
@@ -59,24 +65,24 @@ function showCartList() {
       
               </div>
             </li>`;
-  
-        listCart.innerHTML = htmlContentToAppend;
-      }
+
+      listCart.innerHTML = htmlContentToAppend;
     }
+  }
 }
 
 // Funcion para cambiar la moneda
 function cambioMoneda() {
   var moneda = document.getElementById("productCurrency").value;
-  var preciosS = document.getElementsByClassName("precio-subtotal")
-  var preciosU = document.getElementsByClassName("precio")
-  for(var i = 0; i < preciosS.length; i++){
+  var preciosS = document.getElementsByClassName("precio-subtotal");
+  var preciosU = document.getElementsByClassName("precio");
+  for (var i = 0; i < preciosS.length; i++) {
     var temp = preciosS[i].innerText.split(" ");
-    preciosS[i].innerText = moneda + " " + cambMoneda(temp[0], temp[1], moneda)
+    preciosS[i].innerText = moneda + " " + cambMoneda(temp[0], temp[1], moneda);
   }
-  for(var j = 0; j < preciosU.length; j++){
+  for (var j = 0; j < preciosU.length; j++) {
     var temp = preciosU[j].innerText.split(" ");
-    preciosU[j].innerText = moneda + " " + cambMoneda(temp[0], temp[1], moneda)
+    preciosU[j].innerText = moneda + " " + cambMoneda(temp[0], temp[1], moneda);
   }
   calcTotal();
 }
@@ -88,46 +94,49 @@ function calcTotal() {
   let suma = 0;
   let envio = 0;
   for (let i = 0; i < subs.length; i++) {
-    var temp = subs[i].innerText.split(" ")[1]
-      suma += parseInt(temp)
+    var temp = subs[i].innerText.split(" ")[1];
+    suma += parseInt(temp);
   }
 
   var radios = document.getElementsByName("tipoEnvio");
 
   for (var i = 0, length = radios.length; i < length; i++) {
-      if (radios[i].checked) {
-          envio = suma * parseFloat(radios[i].value);
-          break;
-      }
+    if (radios[i].checked) {
+      envio = suma * parseFloat(radios[i].value);
+      break;
+    }
   }
   var signo = "";
-  if(moneda == "UYU"){
+  if (moneda == "UYU") {
     signo = "$";
   } else {
     signo = "U$S";
   }
-  document.getElementById("subtotalID").innerText = signo + " " + suma.toFixed(2);
+  document.getElementById("subtotalID").innerText =
+    signo + " " + suma.toFixed(2);
   document.getElementById("envioID").innerText = signo + " " + envio.toFixed(2);
-  document.getElementById("totalConEnvioID").innerText = signo + " " + (suma + envio).toFixed(2);
-  document.getElementById("totalConEnvioTextoID").innerText = "Total (" + moneda + ") ";
+  document.getElementById("totalConEnvioID").innerText =
+    signo + " " + (suma + envio).toFixed(2);
+  document.getElementById("totalConEnvioTextoID").innerText =
+    "Total (" + moneda + ") ";
 }
 
 // Funcion para calcular el subtotal
 function calcSubtotal(unitCost, i) {
   if (document.getElementById(`count${i}`) != null) {
-      let count = parseInt(document.getElementById(`count${i}`).value);
-      subtotal = checkCurrency(unitCost, cart_info_array[i].currency) * count;
-      document.getElementById(`productSubtotal${i}`).innerText = subtotal;
-      calcTotal();
+    let count = parseInt(document.getElementById(`count${i}`).value);
+    subtotal = checkCurrency(unitCost, cart_info_array[i].currency) * count;
+    document.getElementById(`productSubtotal${i}`).innerText = subtotal;
+    calcTotal();
   }
 }
 
-// Actualiza el subTotal en base a la cantidad de cada articulo 
+// Actualiza el subTotal en base a la cantidad de cada articulo
 function actualizaCalc(i) {
   var precio = document.getElementById("productCostText" + i);
   var temp = document.getElementById("precio-" + i).innerText.split(" ");
   var cant = document.getElementById("cant" + i).value;
-  precio.innerText = temp[0] + " " + (temp[1] * cant);
+  precio.innerText = temp[0] + " " + temp[1] * cant;
   calcTotal();
 }
 
@@ -137,7 +146,7 @@ function cambMoneda(moneda, cantidad, monedaOut) {
     return cantidad;
   }
   switch (moneda) {
-      case "UYU":
+    case "UYU":
       if (monedaOut == "USD") {
         return cantidad / cotizacion;
       }
@@ -152,24 +161,91 @@ function cambMoneda(moneda, cantidad, monedaOut) {
 
 // Elimina un artículo del carrito
 function descartarArt(num) {
-  var li = document.getElementById("item-"+num)
-  var ul = document.getElementById("listCart").removeChild(li)
+  var li = document.getElementById("item-" + num);
+  var ul = document.getElementById("listCart").removeChild(li);
   calcTotal();
 }
 
+// Selecciono el método de pago
+function metodoPago() {
+  let pagos = document.getElementsByName("tipoDePago");
+  for (let i = 0; i < pagos.length; i++) {
+    if (pagos[i].checked && pagos[i].value == "1") {
+      document.getElementById("modalDefinirTarjeta").classList.remove("d-none");
+      document.getElementById("modalDefinirBanco").classList.add("d-none");
+    } else if (pagos[i].checked && pagos[i].value == "2") {
+      document.getElementById("modalDefinirTarjeta").classList.add("d-none");
+      document.getElementById("modalDefinirBanco").classList.remove("d-none");
+    }
+  }
+}
 
+// Validación para el método de pago
+function agregarMetodoDePago() {
+  metodoDePagoSeleccionado = true;
+  document.getElementById(
+    "selecFormaDePago"
+  ).innerHTML = ` <div class="alert-success mt-3">
+  <strong>Forma de pago ingresada.</strong>
+  </div>
+  `;
+  $("#modalFormaDePago").modal("hide");
+}
 
+function goHome() {
+  location.href="index.html";
+}
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(CART_INFO2_URL).then(function (resultObj) {
-        if (resultObj.status === "ok") {    
-          cart_info_array = resultObj.data.articles;    
-          // Muestra los productos agregados al carrito
-          showCartList();
-          calcTotal();
-        }
-      });
+document.addEventListener("DOMContentLoaded", function (e) {
+  getJSONData(CART_INFO2_URL).then(function (resultObj) {
+    if (resultObj.status === "ok") {
+      cart_info_array = resultObj.data.articles;
+      // Muestra los productos agregados al carrito
+      showCartList();
+      calcTotal();
+    }
+  });
+  // Evento para que no me recargue la página al aceptar un modo de pago.
+  document.getElementById("pagoBanco").addEventListener("submit", function (e) {
+    e.preventDefault();
+    agregarMetodoDePago();
+    return false;
+  });
+
+  document
+    .getElementById("pagoTarjeta")
+    .addEventListener("submit", function (e) {
+      e.preventDefault();
+      agregarMetodoDePago();
+      return false;
+    });
+
+  document
+    .getElementById("needs-validation")
+    .addEventListener("submit", function (e) {
+      e.preventDefault();
+      if (metodoDePagoSeleccionado == false) {
+        document.getElementById(
+          "selecFormaDePago"
+        ).innerHTML = ` <div class="alert-danger mt-3">
+    <strong>Se debe de seleccionar una forma de pago.</strong>
+    </div>
+    `;
+      } else {
+        document.getElementById("carrito").innerHTML = `
+<div class="alert alert-success alert-dismissible show mt-3" role="alert">
+    <strong>Felicidades!</strong>
+    <p>
+        Su compra ha sido realizada con éxitos.
+    </p>
+    <button type="button" class="close" onclick="goHome()" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>
+`;
+      }
+    });
 });
