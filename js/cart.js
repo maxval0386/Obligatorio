fd8261f253631a0f27da39892f400e1c83bac680
maxval0386 <88293942+maxval0386@users.noreply.github.com>
2021-11-14 22:41:50 -0300

    Final entrega 7

--- js/cart.js
+++ js/cart.js
@@ -197,47 +197,77 @@ function goHome() {
   location.href="index.html";
 }
 
-$(document).ready(function(){
+// Formato para tarjeta de crédito
+$(document).ready(function () {
   //Para dar formato al número de la tarjeta.
-  var cardNum = document.getElementById('cr_no');
+  var cardNum = document.getElementById("cr_no");
   cardNum.onkeyup = function (e) {
-  if (this.value == this.lastValue) return;
-  var caretPosition = this.selectionStart;
-  var sanitizedValue = this.value.replace(/[^0-9]/gi, '');
-  var parts = [];
-  
-  for (var i = 0, len = sanitizedValue.length; i < len; i +=4) { parts.push(sanitizedValue.substring(i, i + 4)); } for (var i=caretPosition - 1; i>= 0; i--) {
+    if (this.value == this.lastValue) return;
+    var caretPosition = this.selectionStart;
+    var sanitizedValue = this.value.replace(/[^0-9]/gi, "");
+    var parts = [];
+
+    for (var i = 0, len = sanitizedValue.length; i < len; i += 4) {
+      parts.push(sanitizedValue.substring(i, i + 4));
+    }
+    for (var i = caretPosition - 1; i >= 0; i--) {
       var c = this.value[i];
-      if (c < '0' || c> '9') {
-          caretPosition--;
-          }
-          }
-          caretPosition += Math.floor(caretPosition / 4);
-  
-          this.value = this.lastValue = parts.join('-');
-          this.selectionStart = this.selectionEnd = caretPosition;
-          }
-  
-          //Para dar formato a la fecha de expiración
-          var expDate = document.getElementById('exp');
-          expDate.onkeyup = function (e) {
-          if (this.value == this.lastValue) return;
-          var caretPosition = this.selectionStart;
-          var sanitizedValue = this.value.replace(/[^0-9]/gi, '');
-          var parts = [];
-  
-          for (var i = 0, len = sanitizedValue.length; i < len; i +=2) { parts.push(sanitizedValue.substring(i, i + 2)); } for (var i=caretPosition - 1; i>= 0; i--) {
-              var c = this.value[i];
-              if (c < '0' || c> '9') {
-                  caretPosition--;
-                  }
-                  }
-                  caretPosition += Math.floor(caretPosition / 2);
-  
-                  this.value = this.lastValue = parts.join('/');
-                  this.selectionStart = this.selectionEnd = caretPosition;
-                  }
-                  })
+      if (c < "0" || c > "9") {
+        caretPosition--;
+      }
+    }
+    caretPosition += Math.floor(caretPosition / 4);
+
+    this.value = this.lastValue = parts.join("-");
+    this.selectionStart = this.selectionEnd = caretPosition;
+  };
+
+  //Para dar formato al número del CVC.
+  var cardNum = document.getElementById("cvc");
+  cardNum.onkeyup = function (e) {
+    if (this.value == this.lastValue) return;
+    var caretPosition = this.selectionStart;
+    var sanitizedValue = this.value.replace(/[^0-9]/gi, "");
+    var parts = [];
+
+    for (var i = 0, len = sanitizedValue.length; i < len; i += 4) {
+      parts.push(sanitizedValue.substring(i, i + 4));
+    }
+    for (var i = caretPosition - 1; i >= 0; i--) {
+      var c = this.value[i];
+      if (c < "0" || c > "9") {
+        caretPosition--;
+      }
+    }
+    caretPosition += Math.floor(caretPosition / 4);
+
+    this.value = this.lastValue = parts.join("-");
+    this.selectionStart = this.selectionEnd = caretPosition;
+  };
+
+  //Para dar formato a la fecha de expiración
+  var expDate = document.getElementById("exp");
+  expDate.onkeyup = function (e) {
+    if (this.value == this.lastValue) return;
+    var caretPosition = this.selectionStart;
+    var sanitizedValue = this.value.replace(/[^0-9]/gi, "");
+    var parts = [];
+
+    for (var i = 0, len = sanitizedValue.length; i < len; i += 2) {
+      parts.push(sanitizedValue.substring(i, i + 2));
+    }
+    for (var i = caretPosition - 1; i >= 0; i--) {
+      var c = this.value[i];
+      if (c < "0" || c > "9") {
+        caretPosition--;
+      }
+    }
+    caretPosition += Math.floor(caretPosition / 2);
+
+    this.value = this.lastValue = parts.join("/");
+    this.selectionStart = this.selectionEnd = caretPosition;
+  };
+});
 
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
